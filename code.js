49cf8baf5c4a54dfe452e45b19f10de0940672f0
Javascript <doman.20042020@gmail.com>
2024-03-19 10:29:32 +0700

    Change valid #7 and #8

--- code.js
+++ code.js
@@ -9,12 +9,3 @@ console.log('>>> Check abc:','a = ', a, 'b = ', b, 'c = ', c);
 console.log(typeof a);
 // muốn biết kiểu dử liệu của biến a là gì thù dùng typeof để biết dc kiểu dữ liệu của nó
 
-//!#8
-//Lỗi NaN xảy ra khi bạn đang cố gắng in ra 1 biến nhưng biến đó kh phải là number
-let d = 'Toi la string - chuoi';
-console.log(d)
-
-console.log(4 + 5 + 'a'); // >>> 9a
-console.log('a' + 4 + 5 ); // >>> a45
-// ở 2 đoạn mã log 1 trên nếu phép tính dc đặt đầu tiên thì sẽ là: 4 + 5 = 9 + 'a' = 9a;
-//còn ở log 2 thì nếu có 1 string dc đặt đầu tiên thì sẽ là: 'a' + 4 + 5 = a45, bởi vì string 'a' dc đặt đầu tiên nên a nó sẽ hiểu 45 là 1 chuỗi a45
