244d45a5c9a9f59bf0bda97725310ad53d348c65
Javascript <doman.20042020@gmail.com>
2024-03-19 09:27:24 +0700

    #7

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-alert('Hello guy')
+//#7 Tất Tần Tật Các Kiểu Dữ Liệu Cần Biết của JS
+console.log(`Hoc JS cung hoi dan IT ''"""`); 
+// dòng lệnh này giúp thêm 1 đoạn vb có dấu '',"" bên trong 1 đoạn vb mà kh sợ bị lỗi bởi nó dc bao bọc bởi dấu ``
+
+let a = 6, b = 8, c = 10;
+
+console.log('>>> Check abc:','a = ', a, 'b = ', b, 'c = ', c);
+
+console.log(typeof a);
+// muốn biết kiểu dử liệu của biến a là gì thù dùng typeof để biết dc kiểu dữ liệu của nó
