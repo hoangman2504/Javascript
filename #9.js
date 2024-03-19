4c51e6879c656d6440729c99e85f7a9c9f3a030c
Javascript <doman.20042020@gmail.com>
2024-03-19 11:07:46 +0700

    save on #8

--- #9.js
+++
@@ -1,6 +0,0 @@
-//!#9 - Các Methods Được Sử Dụng với Xờ Trings (Strings)
-
-// Chủ yếu ở bài này chúng ta kh cần phải học hết các methods với string, chủ yếu là
-// chúng ta biết cách search google bằng tiếng anh chứ kh phải tiếng việt,
-// ví dụ với lệnh split dùng để xóa dấu cách thì khi search chúng ta sẽ js split là google
-// sẽ hiển thị kết quả tham khao cho chúng ta thay vì seach bằng tiếng việt
