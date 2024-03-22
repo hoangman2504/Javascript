5907658870c341cc804ff62930f1aa6d14ab56f2
Javascript <doman.20042020@gmail.com>
2024-03-22 10:39:20 +0700

    #15 Js For loops

---
+++ #15.js
@@ -0,0 +1,16 @@
+//!#15 For Loops - Làm Quen Với Vòng Lặp For
+let arr = ['tokino', 'basoka', 'nana', 'kawa'];
+
+//Trong vòng lặp for này đk trong cặp () sẽ dc chia làm 3 phần
+//p1: i = 0 => để khởi tạo 1 biến
+//p2: i < arr.length => đk của vòng lặp
+//p3: i++ => là đk để đoạn lệnh quyết định in ra gì sao khi đã hoàn lặp qua hết các đối tượng
+//note: => ở 3 phần trên thì code sẽ từ p1,p2 xong 2 phần thi p3 sẽ quyết định vòng lặp for in ra gì
+for(i = 0; i < arr.length; i++) {
+    console.log('>>>Check arr: ',arr[i]);
+}
+
+
+
+
+
