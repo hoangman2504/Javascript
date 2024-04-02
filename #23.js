a9aa30fd2741975a3d76cbafb827f3a6faad90e0
Javascript <doman.20042020@gmail.com>
2024-04-02 11:26:51 +0700

    Func and method

--- #23.js
+++ #23.js
@@ -15,5 +15,7 @@ let obj = {
 };
 
 obj.greet();
-
+//note: thắc mắc tại sao greet lại gắn vào 1 func thì do đó là khi nó sẽ gọi obj.greet()
+// thì method ở đây là greet, tóm lại thì biến greet nó có khai báo 1 func để nó lấy hàm name thì
+//nó gọi là 1 method
 //note: method thường sẽ thao tác hoặc truy cập các thuộc tính của đối tượng
